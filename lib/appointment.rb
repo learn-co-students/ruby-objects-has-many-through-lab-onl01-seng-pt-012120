d536eee3bb496f9ad79090d15b6d9d1d3cc834a5
Dan Foley <dan@dan-foley.com>
2020-02-05 19:36:30 +0000

    Done.

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,1 +1,25 @@
 require "pry"
+
+class Appointment
+
+  attr_accessor :doctor, :patient, :date
+  
+  @@all = []
+  
+  def initialize(date, patient, doctor)
+    @date = date
+    @patient = patient
+    @doctor = doctor
+    save
+  end
+  
+  def save
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+
+
+end
