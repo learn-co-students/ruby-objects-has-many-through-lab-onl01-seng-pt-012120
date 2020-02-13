c72b9dee4de6faf5d1f6367beb469558148257fb
Roberto Livi <robertomlivi@outlook.com>
2020-02-13 22:16:50 +0000

    Done.

---
+++ lib/genre.rb
@@ -0,0 +1,25 @@
+class Genre
+  
+  attr_accessor :name
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @songs = []
+    @@all << self
+  end
+  
+  def songs
+    Song.all.select {|song| song.genre == self}
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def artists
+    Song.all.map {|song| song.artist}
+  end 
+  
+end
