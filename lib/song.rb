4c03ae3864db9524a2afbf086c8dc8b821e17154
Daniel Sun <danielsun12345@gmail.com>
2020-02-16 22:58:55 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -2,7 +2,7 @@ class Song
   attr_accessor :name, :artist, :genre 
   
   @@all = []
-  def initialize(name, genre)
+  def initialize(name, artist, genre)
     @name = name
     @artist = artist
     @genre = genre 
