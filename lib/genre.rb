236751d8a4f2fd882ca6028e47b5e8e9a0e59e95
Karen Daniela <kdaniimora@gmail.com>
2020-02-21 22:33:05 +0000

    Done.

---
+++ lib/genre.rb
@@ -0,0 +1,23 @@
+class Genre 
+  attr_accessor :name 
+  
+    @@all = []
+  def initialize(name)
+    @name = name 
+    @@all << self 
+end
+  
+ def self.all 
+    @@all 
+end 
+
+def songs 
+  Song.all {|songs| songs.genre == self}
+end 
+
+def artists 
+  self.song.map {|song| song.artist}
+end 
+
+
+end 
