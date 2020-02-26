893e440ffa16a5093ad9499c96f279d39c917c2e
Derric Fragosa <fderric@gmail.com>
2020-02-26 22:43:04 +0000

    Done.

---
+++ lib/patient.rb
@@ -0,0 +1,32 @@
+require 'pry'
+class Patient
+  attr_accessor :name
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def new_appointment(doctor, date)
+    appt = Appointment.new(name)
+    appt.patient == self
+  end
+  
+  def appointments
+    
+  end
+  
+  def doctors
+    
+  end
+  
+  
+  
+  
+end
